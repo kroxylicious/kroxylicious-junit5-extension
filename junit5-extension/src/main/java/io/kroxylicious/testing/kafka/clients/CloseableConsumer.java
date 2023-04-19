2000c4f1a2a49271a24f7482ad9a3db8f00a3623
Francisco Vila <57452611+franvila@users.noreply.github.com>
2023-04-19 07:18:22 +0200

    Update junit5-extension/src/main/java/io/kroxylicious/testing/kafka/clients/CloseableConsumer.java
    Co-authored-by: Sam Barker <sam@quadrocket.co.uk>
    Signed-off-by: Francisco Vila <57452611+franvila@users.noreply.github.com>

--- junit5-extension/src/main/java/io/kroxylicious/testing/kafka/clients/CloseableConsumer.java
+++ junit5-extension/src/main/java/io/kroxylicious/testing/kafka/clients/CloseableConsumer.java
@@ -27,7 +27,7 @@ import org.apache.kafka.common.PartitionInfo;
 import org.apache.kafka.common.TopicPartition;
 
 /**
- * The type Closeable consumer.
+ * Provides a simple wrapper around a Kafka Consumer to redirect `close()` so that it has a sensible timeout and can thus be safely used in a try-with-resources block. All other methods delegate to the wrapped Consumer client. 
  *
  * @param instance the consumer instance
  * @param <K>  the type parameter
