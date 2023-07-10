56a9a5cd09aa2be76219b2a93ada2c7f1dac9ffa
kwall <kwall@apache.org>
2023-07-10 16:43:30 +0100

    Direct storage formatting stdout to the log

---
+++ impl/src/main/java/io/kroxylicious/testing/kafka/invm/LoggingPrintStream.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright Kroxylicious Authors.
+ *
+ * Licensed under the Apache Software License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+package io.kroxylicious.testing.kafka.invm;
+
+import java.io.ByteArrayOutputStream;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.nio.charset.Charset;
+
+final class LoggingPrintStream {
+
+    static PrintStream loggingPrintStream(System.Logger logger, System.Logger.Level level) {
+        return new PrintStream(new LoggingOutputStream(logger, level));
+    }
+
+    private LoggingPrintStream() {
+    }
+
+    private static class LoggingOutputStream extends OutputStream {
+        private final ByteArrayOutputStream os = new ByteArrayOutputStream(1000);
+        private final System.Logger logger;
+        private final System.Logger.Level level;
+
+        public LoggingOutputStream(System.Logger logger, System.Logger.Level level) {
+            this.logger = logger;
+            this.level = level;
+        }
+
+        @Override
+        public void write(int b) {
+            if (b == '\n' || b == '\r') {
+                logPending();
+            }
+            else {
+                os.write(b);
+            }
+        }
+
+        @Override
+        public void close() {
+            logPending();
+        }
+
+        private void logPending() {
+            var log = os.toString(Charset.defaultCharset()).stripTrailing();
+            if (!log.isEmpty()) {
+                logger.log(level, "{0}", log);
+            }
+            os.reset();
+        }
+    }
+
+}
---
+++ impl/src/test/java/io/kroxylicious/testing/kafka/invm/LoggingPrintStreamTest.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright Kroxylicious Authors.
+ *
+ * Licensed under the Apache Software License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+package io.kroxylicious.testing.kafka.invm;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+
+@ExtendWith(MockitoExtension.class)
+class LoggingPrintStreamTest {
+
+    @Mock
+    private System.Logger logger;
+
+    @Test
+    void singleLog() {
+        try (var lps = LoggingPrintStream.loggingPrintStream(logger, System.Logger.Level.INFO)) {
+            lps.println("Hello, world");
+
+            verify(logger, times(1)).log(System.Logger.Level.INFO, "{0}", "Hello, world");
+        }
+        verifyNoMoreInteractions(logger);
+    }
+
+    @Test
+
+    void singleLogGeneratedByManyPrints() {
+        try (var lps = LoggingPrintStream.loggingPrintStream(logger, System.Logger.Level.INFO)) {
+            lps.print("Hello,");
+            lps.println(" world");
+
+            verify(logger, times(1)).log(System.Logger.Level.INFO, "{0}", "Hello, world");
+        }
+        verifyNoMoreInteractions(logger);
+    }
+
+    @Test
+    void twoLogs() {
+        try (var lps = LoggingPrintStream.loggingPrintStream(logger, System.Logger.Level.INFO)) {
+            lps.println("Hello, world");
+            lps.println("Goodbye, cruel world");
+            verify(logger, times(1)).log(System.Logger.Level.INFO, "{0}", "Hello, world");
+            verify(logger, times(1)).log(System.Logger.Level.INFO, "{0}", "Goodbye, cruel world");
+        }
+        verifyNoMoreInteractions(logger);
+    }
+
+    @Test
+    void closeLogsUnfinishedPrint() {
+        try (var lps = LoggingPrintStream.loggingPrintStream(logger, System.Logger.Level.INFO)) {
+            lps.print("There's no newline");
+        }
+
+        verify(logger, times(1)).log(System.Logger.Level.INFO, "{0}", "There's no newline");
+        verifyNoMoreInteractions(logger);
+    }
+
+}
