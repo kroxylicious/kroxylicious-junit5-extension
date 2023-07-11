kroxylicious/kroxylicious-junit5-extension

commit 3cd6816bfa71b0961e6eb7d4616af8d038c4b0be

Improve Utils#createTopic (#142)
* Improve Utils#createTopic
- avoid ignoring the result of the recursive call
- avoid the tight spin that occurs whilst awaiting brokers to mesh.
- silences the FutureReturnValueIgnored complaint from sonatype-lift

* Update CHANGELOG.md

impl/src/main/java/io/kroxylicious/testing/kafka/common/Utils.java
@@ -15,6 +15,7 @@ import java.util.Optional;
 import java.util.Set;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionException;
+import java.util.concurrent.CompletionStage;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
@@ -24,7 +25,6 @@ import org.apache.kafka.clients.admin.Admin;
 import org.apache.kafka.clients.admin.NewPartitionReassignment;
 import org.apache.kafka.clients.admin.NewTopic;
 import org.apache.kafka.clients.admin.TopicDescription;
-import org.apache.kafka.common.KafkaFuture;
 import org.apache.kafka.common.TopicPartition;
 import org.apache.kafka.common.TopicPartitionInfo;
 import org.apache.kafka.common.errors.InvalidReplicationFactorException;
@@ -34,7 +34,6 @@ import org.apache.kafka.common.errors.UnknownTopicOrPartitionException;
 import org.apache.kafka.common.internals.Topic;
 import org.awaitility.Awaitility;
 import org.awaitility.core.ConditionFactory;
-import org.junit.Assert;
 import org.slf4j.Logger;
 
 import static org.apache.kafka.clients.CommonClientConfigs.BOOTSTRAP_SERVERS_CONFIG;
@@ -123,7 +122,7 @@ public class Utils {
             log.debug("Creating topic: {} via {}", CONSISTENCY_TEST, connectionConfig.get(BOOTSTRAP_SERVERS_CONFIG));
             // Note we don't wait for the Topic to be created, we assume it will complete eventually.
             // As the thing we care about is it actually being replicated to the other brokers anyway so we wait to confirm replication.
-            createTopic(expectedBrokerCount, admin);
+            var ignored = createTopic(expectedBrokerCount, admin);
             log.debug("Waiting for {} to be replicated to {} brokers", CONSISTENCY_TEST, expectedBrokerCount);
             awaitCondition(timeout, timeUnit)
                     .until(() -> {
@@ -199,21 +198,22 @@ public class Utils {
         promise.complete(false);
     }
 
-    private static KafkaFuture<Void> createTopic(Integer expectedBrokerCount, Admin admin) {
+    private static CompletionStage<Void> createTopic(Integer expectedBrokerCount, Admin admin) {
         return admin.createTopics(Set.of(new NewTopic(CONSISTENCY_TEST, 1, expectedBrokerCount.shortValue())))
                 .all()
-                .whenComplete((unused, throwable) -> {
-                    log.debug("Create topic future completed.");
-                    if (throwable != null) {
-                        log.warn("Failed to create topic: {} due to {}", CONSISTENCY_TEST, throwable.getMessage(), throwable);
-                        if (throwable instanceof RetriableException || throwable instanceof InvalidReplicationFactorException) {
-                            CompletableFuture.supplyAsync(() -> createTopic(expectedBrokerCount, admin));
-                        }
-                        else {
-                            Assert.fail("Failed to create topic: " + CONSISTENCY_TEST + "  due to " + throwable.getMessage());
-                        }
+                .toCompletionStage()
+                .thenRun(() -> log.debug("Create future for topic {} completed.", CONSISTENCY_TEST))
+                .exceptionallyComposeAsync((throwable) -> {
+                    log.warn("Failed to create topic: {} due to {}", CONSISTENCY_TEST, throwable.getMessage());
+                    if (throwable instanceof RetriableException || throwable instanceof InvalidReplicationFactorException) {
+                        // Retry the creation of the topic. The delayed executor used in this stage's handling avoids
+                        // a tight spinning loop.
+                        return createTopic(expectedBrokerCount, admin);
                     }
-                });
+                    else {
+                        return CompletableFuture.failedStage(new RuntimeException("Failed to create topic: " + CONSISTENCY_TEST, throwable));
+                    }
+                }, CompletableFuture.delayedExecutor(100, TimeUnit.MILLISECONDS));
     }
 
     /**

junit5-extension/src/test/java/io/kroxylicious/testing/kafka/junit5ext/BeforeEachTest.java
@@ -5,15 +5,16 @@
  */
 package io.kroxylicious.testing.kafka.junit5ext;
 
-import io.kroxylicious.testing.kafka.api.KafkaCluster;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
 import org.apache.kafka.clients.admin.Admin;
 import org.apache.kafka.clients.admin.NewTopic;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
-import java.util.List;
-import java.util.concurrent.TimeUnit;
+import io.kroxylicious.testing.kafka.api.KafkaCluster;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.testcontainers.shaded.org.awaitility.Awaitility.await;

commit fed77e59b249a5189bc2dd71db3668c1de35d085

Fix regression caused by #142 (#144)
* github workflow: retain container-logs after an error

* Fix regression introduced into Utils#createTopic by 3cd6816bfa71b0961e6eb7d4616af8d038c4b0be

* Regression meant that the create topic looping was broken.  Exceptions occurring during stage handlers are propagated to subsequent handlers wrapped by a `CompletionException`.  The code forgot to handle this case.
* Use failFast to short circuit awaitExpectedBrokerCountInClusterViaTopic if the createTopic fails unexpectedly.  This will reveal the error that caused createTopic to be visible to us

.github/workflows/maven.yml
@@ -50,3 +50,10 @@ jobs:
 
       - name: 'Run tests in containers'
         run: TEST_CLUSTER_EXECUTION_MODE=CONTAINER KAFKA_VERSION=latest mvn -B clean verify -Dsurefire.failIfNoSpecifiedTests=false -Dtest=KafkaClusterTest
+
+      - name: Archive container logs
+        uses: actions/upload-artifact@v3
+        if: failure()
+        with:
+          name: container-logs
+          path: "**/container-logs/**/*.log"

impl/src/main/java/io/kroxylicious/testing/kafka/common/Utils.java
@@ -121,10 +121,16 @@ public class Utils {
         try (var admin = Admin.create(connectionConfig)) {
             log.debug("Creating topic: {} via {}", CONSISTENCY_TEST, connectionConfig.get(BOOTSTRAP_SERVERS_CONFIG));
             // Note we don't wait for the Topic to be created, we assume it will complete eventually.
-            // As the thing we care about is it actually being replicated to the other brokers anyway so we wait to confirm replication.
-            var ignored = createTopic(expectedBrokerCount, admin);
+            // As the thing we care about is it actually being replicated to the other brokers anyway, so we wait to confirm replication.
+            var createTopicStage = createTopic(expectedBrokerCount, admin);
             log.debug("Waiting for {} to be replicated to {} brokers", CONSISTENCY_TEST, expectedBrokerCount);
             awaitCondition(timeout, timeUnit)
+                    .failFast(() -> {
+                        var f = createTopicStage.toCompletableFuture();
+                        if (f.isCompletedExceptionally()) {
+                            f.get();
+                        }
+                    })
                     .until(() -> {
                         log.debug("Calling describe topic");
                         final var promise = new CompletableFuture<Boolean>();
@@ -205,6 +211,9 @@ public class Utils {
                 .thenRun(() -> log.debug("Create future for topic {} completed.", CONSISTENCY_TEST))
                 .exceptionallyComposeAsync((throwable) -> {
                     log.warn("Failed to create topic: {} due to {}", CONSISTENCY_TEST, throwable.getMessage());
+                    if (throwable instanceof CompletionException && throwable.getCause() != null) {
+                        throwable = throwable.getCause();
+                    }
                     if (throwable instanceof RetriableException || throwable instanceof InvalidReplicationFactorException) {
                         // Retry the creation of the topic. The delayed executor used in this stage's handling avoids
                         // a tight spinning loop.