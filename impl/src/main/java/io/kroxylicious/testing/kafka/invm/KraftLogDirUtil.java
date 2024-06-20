a7880c11d35804df662eb864c475fc0913095d15
kwall <kwall@apache.org>
2024-06-20 10:49:16 +0100

    fallback to old formatCommand to support older brokers.

--- impl/src/main/java/io/kroxylicious/testing/kafka/invm/KraftLogDirUtil.java
+++ impl/src/main/java/io/kroxylicious/testing/kafka/invm/KraftLogDirUtil.java
@@ -26,6 +26,7 @@ import scala.collection.immutable.Seq;
  */
 final class KraftLogDirUtil {
     private static final PrintStream LOGGING_PRINT_STREAM = LoggingPrintStream.loggingPrintStream(InVMKafkaCluster.LOGGER, System.Logger.Level.DEBUG);
+    private static final String FORMAT_METHOD_NAME = "formatCommand";
     private static final boolean IGNORE_FORMATTED = true;
 
     private KraftLogDirUtil() {
@@ -53,12 +54,21 @@ final class KraftLogDirUtil {
 
     private static void formatReflectively(Object metaProperties, Seq<String> directories, BootstrapMetadata bootstrapMetadata, MetadataVersion metadataVersion)
             throws NoSuchMethodException, IllegalAccessException, InvocationTargetException {
-        var formatMethod = StorageTool.class.getDeclaredMethod("formatCommand", PrintStream.class, Seq.class, metaProperties.getClass(), BootstrapMetadata.class,
-                MetadataVersion.class,
-                boolean.class);
-        // note ignoreFormatter=true so tolerate a log directory which is already formatted. this is
-        // required to support start/stop.
-        formatMethod.invoke(null, LOGGING_PRINT_STREAM, directories, metaProperties, bootstrapMetadata, metadataVersion, IGNORE_FORMATTED);
+        try {
+            var formatMethod = StorageTool.class.getDeclaredMethod(FORMAT_METHOD_NAME, PrintStream.class, Seq.class, metaProperties.getClass(), BootstrapMetadata.class,
+                    MetadataVersion.class,
+                    boolean.class);
+            // note ignoreFormatter=true so tolerate a log directory which is already formatted. this is
+            // required to support start/stop.
+            formatMethod.invoke(null, LOGGING_PRINT_STREAM, directories, metaProperties, bootstrapMetadata, metadataVersion, IGNORE_FORMATTED);
+        }
+        catch (NoSuchMethodException e) {
+            // fallback for pre-kafka 3.6 formatCommand which didn't accept BootstrapMetadata
+            var formatMethod = StorageTool.class.getDeclaredMethod(FORMAT_METHOD_NAME, PrintStream.class, Seq.class, metaProperties.getClass(),
+                    MetadataVersion.class,
+                    boolean.class);
+            formatMethod.invoke(null, LOGGING_PRINT_STREAM, directories, metaProperties, metadataVersion, IGNORE_FORMATTED);
+        }
     }
 
     private static BootstrapMetadata buildBootstrapMetadata(List<UserScramCredentialRecord> scramCredentialRecords, MetadataVersion metadataVersion) {
