367127a98369ab6339ab2b1c6edc0dbd74cecb29
Robert Young <robertyoungnz@gmail.com>
2025-08-12 10:47:38 +1200

    Use admin client to create scram users
    Instead of specialcasing v4.1+ we can use the same mechanism to create
    scram users for all clusters. Instead of injecting them into the
    metadata store at KRaft storage formatting time, or into ZK using a ZK
    client, we use the Kafka admin client to create the scram credentials.
    
    Signed-off-by: Robert Young <robertyoungnz@gmail.com>

---
+++ impl/src/main/java/io/kroxylicious/testing/kafka/common/ScramInitialiser.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright Kroxylicious Authors.
+ *
+ * Licensed under the Apache Software License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
+ */
+package io.kroxylicious.testing.kafka.common;
+
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+
+import org.apache.kafka.clients.admin.Admin;
+import org.apache.kafka.clients.admin.ScramCredentialInfo;
+import org.apache.kafka.clients.admin.ScramMechanism;
+import org.apache.kafka.clients.admin.UserScramCredentialUpsertion;
+import org.apache.kafka.clients.admin.UserScramCredentialsDescription;
+import org.awaitility.Awaitility;
+
+import io.kroxylicious.testing.kafka.internal.AdminSource;
+
+import static org.junit.Assert.assertTrue;
+
+public class ScramInitialiser {
+
+    public static void initialiseScramUsers(AdminSource adminSource, KafkaClusterConfig clusterConfig) {
+        try {
+            if (clusterConfig.isSaslScram() && !clusterConfig.getUsers().isEmpty()) {
+                ScramMechanism mechanism = clusterConfig.getScramMechanism()
+                        .orElseThrow(() -> new RuntimeException("config is SASL scram, but scram mechanism is empty"));
+                try (Admin admin = adminSource.createAdmin()) {
+                    admin.alterUserScramCredentials(clusterConfig.getUsers().entrySet().stream()
+                            .map(userEntry -> new UserScramCredentialUpsertion(userEntry.getKey(), new ScramCredentialInfo(mechanism, 4096), userEntry.getValue()))
+                            .collect(Collectors.toList()))
+                            .all().get(5, TimeUnit.SECONDS);
+                    List<String> users = clusterConfig.getUsers().keySet().stream().toList();
+                    // there is a delay in availability, if we don't pause here then some tests that immediately try to produce records fail to authenticate
+                    Awaitility.await().atMost(10, TimeUnit.SECONDS).untilAsserted(() -> {
+                        Map<String, UserScramCredentialsDescription> scramDescription = admin.describeUserScramCredentials(users).all()
+                                .get(5, TimeUnit.SECONDS);
+                        assertTrue(users.stream().allMatch(scramDescription::containsKey));
+                    });
+                }
+            }
+        }
+        catch (Exception e) {
+            throw new RuntimeException("Failed to initialise scram users", e);
+        }
+    }
+}
--- impl/src/test/java/io/kroxylicious/testing/kafka/testcontainers/TestcontainersKafkaClusterTest.java
+++ impl/src/test/java/io/kroxylicious/testing/kafka/testcontainers/TestcontainersKafkaClusterTest.java
@@ -5,19 +5,30 @@
  */
 package io.kroxylicious.testing.kafka.testcontainers;
 
+import java.util.Map;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Stream;
 
+import org.apache.kafka.clients.CommonClientConfigs;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
+import org.apache.kafka.common.config.SaslConfigs;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.assertj.core.api.InstanceOfAssertFactories;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInfo;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.junitpioneer.jupiter.SetEnvironmentVariable;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.images.RemoteDockerImage;
 import org.testcontainers.utility.DockerImageName;
 
+import io.kroxylicious.testing.kafka.clients.CloseableProducer;
 import io.kroxylicious.testing.kafka.common.KafkaClusterConfig;
 import io.kroxylicious.testing.kafka.common.Version;
 
@@ -242,6 +253,29 @@ class TestcontainersKafkaClusterTest {
                         // this works because the classpath will be 3.9.0 or greater so we are forcing a fallback
                         assertThat(pullPolicyForImage.getVersionPart()).isEqualTo("latest-zookeeper-3.7.0");
                     });
+        }
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = { "4.1.0", "4.0.0", "3.9.0", "3.8.0" })
+    void scramUsersCreated(String version) {
+        KafkaClusterConfig config = clusterConfigBuilder.kafkaVersion(version).securityProtocol("SASL_PLAINTEXT").saslMechanism("SCRAM-SHA-256")
+                .user("admin", "admin-secret").build();
+        try (TestcontainersKafkaCluster testcontainersKafkaCluster = new TestcontainersKafkaCluster(config)) {
+            testcontainersKafkaCluster.start();
+            Map<String, Object> kafkaClientConfiguration = testcontainersKafkaCluster.getKafkaClientConfiguration();
+            assertThat(kafkaClientConfiguration).containsEntry(CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, "SASL_PLAINTEXT");
+            assertThat(kafkaClientConfiguration).containsEntry(SaslConfigs.SASL_MECHANISM, "SCRAM-SHA-256");
+            kafkaClientConfiguration.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+            kafkaClientConfiguration.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+            try (var producer = CloseableProducer.create(kafkaClientConfiguration)) {
+                Future<RecordMetadata> metadataFuture = producer.send(new ProducerRecord<>("topic", "key", "value"));
+                RecordMetadata recordMetadata = metadataFuture.get(10, TimeUnit.SECONDS);
+                assertThat(recordMetadata).isNotNull();
+            }
+        }
+        catch (Exception e) {
+            throw new RuntimeException(e);
         }
     }
 
