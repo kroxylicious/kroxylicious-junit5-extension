b2bb4b68d21f865104ee637ac7a9dd3ff326f5c4
Francisco Vila <fvila@redhat.com>
2025-11-06 12:34:56 +0100

    deleted interface, add new method to generate the certificate file and password
    Signed-off-by: Francisco Vila <fvila@redhat.com>

--- impl/src/main/java/io/kroxylicious/testing/kafka/common/KeystoreManager.java
+++ impl/src/main/java/io/kroxylicious/testing/kafka/common/KeystoreManager.java
@@ -5,23 +5,76 @@
  */
 package io.kroxylicious.testing.kafka.common;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.URI;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.attribute.FileAttribute;
+import java.nio.file.attribute.PosixFilePermission;
+import java.nio.file.attribute.PosixFilePermissions;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.UUID;
+
+import edu.umd.cs.findbugs.annotations.NonNull;
 import io.netty.pkitesting.CertificateBuilder;
 import io.netty.pkitesting.X509Bundle;
+import org.bouncycastle.util.IPAddress;
 
-public interface KeystoreManager {
+public class KeystoreManager {
+    private String password;
 
     /**
      * Creates a CertificateBuilder with the appropriate default values for kroxylicious test usage.
-     * @return The partially populated certificate builder.
+     * @param distinguishedName the distinguished name. See {@link KeystoreManager#buildDistinguishedName(String, String, String, String, String, String, String)}
+     *                          for generating a distinguished name
+     * @return  The partially populated certificate builder.
      */
-    CertificateBuilder newCertificateBuilder();
+    public CertificateBuilder newCertificateBuilder(String distinguishedName) {
+        return newCertificateBuilder(distinguishedName, new ArrayList<>());
+    }
+
+    /**
+     * Creates a CertificateBuilder with the appropriate default values for kroxylicious test usage.
+     * @param distinguishedName the distinguished name. See {@link KeystoreManager#buildDistinguishedName(String, String, String, String, String, String, String)}
+     *                          for generating a distinguished name
+     * @param sanNames the names for SAN: They can be DNS names and/or IP Addresses
+     * @return  The partially populated certificate builder.
+     */
+    public CertificateBuilder newCertificateBuilder(String distinguishedName, @NonNull List<String> sanNames) {
+        CertificateBuilder certificateBuilder = new CertificateBuilder()
+                .rsa2048()
+                .subject(distinguishedName);
+
+        sanNames.forEach(name -> {
+            if (IPAddress.isValidIPv4(name) || IPAddress.isValidIPv6(name)) {
+                certificateBuilder.addSanIpAddress(name);
+            }
+            else {
+                certificateBuilder.addSanDnsName(name);
+            }
+        });
+
+        return certificateBuilder;
+    }
 
     /**
      * Builds and adds provided certificate builder as a self-signed certificate
      * @param certificateBuilder the builder configuring the certificate
      * @return the self-signed certificate.
      */
-    X509Bundle createSelfSignedCertificate(CertificateBuilder certificateBuilder) throws Exception;
+    public X509Bundle createSelfSignedCertificate(CertificateBuilder certificateBuilder) throws Exception {
+        return certificateBuilder.copy()
+                .setIsCertificateAuthority(true)
+                .buildSelfSigned();
+    }
 
     /**
      * Optional we don't need this today!
@@ -29,16 +82,60 @@ public interface KeystoreManager {
      * @param certificateBuilder the builder configuring the certificate
      * @return the signed certificate.
      */
-    X509Bundle createSignedCertificate(X509Bundle issuer, CertificateBuilder certificateBuilder) throws Exception;
+    public X509Bundle createSignedCertificate(X509Bundle issuer, CertificateBuilder certificateBuilder) throws Exception {
+        return certificateBuilder.copy()
+                .buildIssuedBy(issuer);
+    }
 
     /**
      * Formats the provided fields into a RFC5280 compliant form
      * @param email the email
+     * @param domain the domain
      * @param organizationUnit the organization unit
      * @param organization the organization
      * @param city the city
      * @param state the state
      * @param country the country
+     * @return the distinguished name
      */
-    String buildDistinguishedName(String email, String domain, String organizationUnit, String organization, String city, String state, String country);
+    public String buildDistinguishedName(String email, String domain, String organizationUnit, String organization, String city, String state, String country) {
+        return "CN=" + domain + ", OU=" + organizationUnit + ", O=" + organization + ", L=" + city + ", ST=" + state + ", C=" + country + ", EMAILADDRESS=" + email;
+    }
+
+    /**
+     * Gets password.
+     *
+     * @return  the password
+     */
+    public String getPassword() {
+        if (this.password == null) {
+            this.password = UUID.randomUUID().toString().replace("-", "");
+        }
+
+        return this.password;
+    }
+
+    /**
+     * Generate certificate file path. Password can be obtained
+     *
+     * @param bundle the bundle
+     * @return  the path of the generated certificate file
+     * @throws KeyStoreException the key store exception
+     * @throws IOException the io exception
+     * @throws CertificateException the certificate exception
+     * @throws NoSuchAlgorithmException the no such algorithm exception
+     */
+    public Path generateCertificateFile(X509Bundle bundle) throws KeyStoreException, IOException, CertificateException, NoSuchAlgorithmException {
+        KeyStore keyStore = bundle.toKeyStore(getPassword().toCharArray());
+        FileAttribute<Set<PosixFilePermission>> attr = PosixFilePermissions.asFileAttribute(PosixFilePermissions.fromString("rwx------"));
+        Path certsDirectory = Files.createTempDirectory("kroxylicious", attr);
+        Path keyStoreFilePath = Paths.get(certsDirectory.toAbsolutePath().toString(), "keystore.jks");
+        try (FileOutputStream stream = new FileOutputStream(keyStoreFilePath.toFile())) {
+            keyStore.store(stream, getPassword().toCharArray());
+        }
+        keyStoreFilePath.toFile().deleteOnExit();
+        certsDirectory.toFile().deleteOnExit();
+
+        return keyStoreFilePath;
+    }
 }
--- impl/src/test/java/io/kroxylicious/testing/kafka/common/KeystoreTest.java
+++ impl/src/test/java/io/kroxylicious/testing/kafka/common/KeystoreTest.java
@@ -6,9 +6,14 @@
 
 package io.kroxylicious.testing.kafka.common;
 
+import java.nio.file.Path;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
 import java.security.cert.X509Certificate;
+import java.util.ArrayList;
 import java.util.List;
 
+import edu.umd.cs.findbugs.annotations.NonNull;
 import org.assertj.core.api.InstanceOfAssertFactories;
 import org.junit.jupiter.api.Test;
 
@@ -24,24 +29,9 @@ class KeystoreTest {
 
     @Test
     void generateSelfSignedKeyStore() throws Exception {
-        var keystore = new Keystore();
-        CertificateBuilder certificateBuilder = keystore.newCertificateBuilder()
-                .subject(keystore.buildDistinguishedName("test@kroxylicious.io", "localhost", "Dev",
+        var keystore = new KeystoreManager();
+        CertificateBuilder certificateBuilder = keystore.newCertificateBuilder(keystore.buildDistinguishedName("test@kroxylicious.io", "localhost", "Dev",
                         "Kroxylicious.io", null, null, "US"));
-        X509Bundle bundle = keystore.createSelfSignedCertificate(certificateBuilder);
-
-        assertAll(() -> {
-            assertThat(bundle.getCertificate()).isNotNull();
-            assertThat(bundle.isSelfSigned()).isTrue();
-            assertThat(bundle.isCertificateAuthority()).isTrue();
-        });
-    }
-
-    @Test
-    void generateSelfSignedKeyStoreAddingDistinguishedName() throws Exception {
-        var keystore = new Keystore();
-        CertificateBuilder certificateBuilder = keystore.newCertificateBuilder(keystore.buildDistinguishedName("test@kroxylicious.io", "localhost",
-                "Dev", "Kroxylicious.io", null, null, "US"));
         X509Bundle bundle = keystore.createSelfSignedCertificate(certificateBuilder);
 
         assertAll(() -> {
@@ -53,17 +43,20 @@ class KeystoreTest {
 
     @Test
     void generateSignedKeyStore() throws Exception {
-        var keystore = new Keystore();
-        CertificateBuilder certificateBuilder = keystore.newCertificateBuilder()
-                .subject(keystore.buildDistinguishedName("test@kroxylicious.io", "localhost", "Dev",
-                        "Kroxylicious.io", null, null, "US"));
-        X509Bundle issuer = keystore.createSelfSignedCertificate(certificateBuilder);
+        var keystore = new KeystoreManager();
+        CertificateBuilder issuerCertificateBuilder = keystore.newCertificateBuilder(keystore.buildDistinguishedName("mail@kroxylicious.io", "localhost", "TestCA",
+                        "Issuer", null, null, "US"));
+
+        CertificateBuilder certificateBuilder = keystore.newCertificateBuilder(keystore.buildDistinguishedName("test@kroxylicious.io", "localhost", "Dev",
+                "Kroxylicious.io", null, null, "US"));
+
+        X509Bundle issuer = keystore.createSelfSignedCertificate(issuerCertificateBuilder);
         X509Bundle signed = keystore.createSignedCertificate(issuer, certificateBuilder);
 
         assertAll(() -> {
             assertThat(signed.getCertificate()).isNotNull();
             assertThat(signed.getCertificate().getIssuerX500Principal()).isEqualTo(issuer.getCertificate().getSubjectX500Principal());
-            assertThat(signed.isSelfSigned()).isTrue();
+            assertThat(signed.isSelfSigned()).isFalse();
             assertThat(signed.isCertificateAuthority()).isFalse();
         });
     }
@@ -71,11 +64,9 @@ class KeystoreTest {
     @Test
     void generateKeyStoreWithIPDomain() throws Exception {
         String domainIP = "127.0.0.1";
-        var keystore = new Keystore();
-        CertificateBuilder certificateBuilder = keystore.newCertificateBuilder()
-                .subject(keystore.buildDistinguishedName("test@kroxylicious.io", "localhost", "Dev",
-                        "Kroxylicious.io", null, null, "US"))
-                .addSanIpAddress(domainIP);
+        var keystore = new KeystoreManager();
+        CertificateBuilder certificateBuilder = keystore.newCertificateBuilder(keystore.buildDistinguishedName("test@kroxylicious.io", "localhost", "Dev",
+                        "Kroxylicious.io", null, null, "US"), List.of(domainIP));
         X509Bundle bundle = keystore.createSelfSignedCertificate(certificateBuilder);
 
         List<?> expectedIp = List.of(KeystoreTest.ASN_GENERAL_NAME_IP_ADDRESS, domainIP);
@@ -93,11 +84,9 @@ class KeystoreTest {
     @Test
     void generateKeyStoreWithDnsSAN() throws Exception {
         String domain = "localhost";
-        var keystore = new Keystore();
-        CertificateBuilder certificateBuilder = keystore.newCertificateBuilder()
-                .subject(keystore.buildDistinguishedName("test@kroxylicious.io", "localhost", "Dev",
-                        "Kroxylicious.io", null, null, "US"))
-                .addSanDnsName(domain);
+        var keystore = new KeystoreManager();
+        CertificateBuilder certificateBuilder = keystore.newCertificateBuilder(keystore.buildDistinguishedName("test@kroxylicious.io", "localhost", "Dev",
+                        "Kroxylicious.io", null, null, "US"), List.of(domain));
         X509Bundle bundle = keystore.createSelfSignedCertificate(certificateBuilder);
 
         List<?> expectedDns = List.of(KeystoreTest.ASN_GENERAL_NAME_DNS, domain);
@@ -116,12 +105,9 @@ class KeystoreTest {
     void generatesKeyStoreWithIPAndDnsSAN() throws Exception {
         String domain = "localhost";
         String domainIP = "127.0.0.1";
-        var keystore = new Keystore();
-        CertificateBuilder certificateBuilder = keystore.newCertificateBuilder()
-                .subject(keystore.buildDistinguishedName("test@kroxylicious.io", "localhost", "Dev",
-                        "Kroxylicious.io", null, null, "US"))
-                .addSanIpAddress(domainIP)
-                .addSanDnsName(domain);
+        var keystore = new KeystoreManager();
+        CertificateBuilder certificateBuilder = keystore.newCertificateBuilder(keystore.buildDistinguishedName("test@kroxylicious.io", "localhost", "Dev",
+                        "Kroxylicious.io", null, null, "US"), List.of(domain, domainIP));
 
         X509Bundle bundle = keystore.createSelfSignedCertificate(certificateBuilder);
 
@@ -136,5 +122,71 @@ class KeystoreTest {
                             .anyMatch(x -> x.equals(expectedDns))
                             .anyMatch(x -> x.equals(expectedIp));
                 });
+    }
+
+    @Test
+    void generateKeyStoreFile() throws Exception {
+        String domain = "localhost";
+        var keystore = new KeystoreManager();
+        CertificateBuilder issuerCertificateBuilder = keystore.newCertificateBuilder(keystore.buildDistinguishedName("mail@kroxylicious.io", domain, "TestCA",
+                "Issuer", null, null, "US"));
+
+        CertificateBuilder certificateBuilder = keystore.newCertificateBuilder(keystore.buildDistinguishedName("test@kroxylicious.io", domain, "Dev",
+                "Kroxylicious.io", null, null, "US"));
+
+        X509Bundle issuer = keystore.createSelfSignedCertificate(issuerCertificateBuilder);
+        X509Bundle signed = keystore.createSignedCertificate(issuer, certificateBuilder);
+
+        String password = keystore.getPassword();
+        Path keyStoreFilePath = keystore.generateCertificateFile(signed);
+        assertThat(keyStoreFilePath.toFile()).exists();
+        var ks = KeyStore.getInstance(keyStoreFilePath.toFile(), password.toCharArray());
+        var keyAliases = keyAliasList(ks);
+        assertThat(keyAliases).hasSize(1);
+        var keyAlias = keyAliases.get(0);
+
+        var certAliases = certificateAliasList(ks);
+        var certAlias = certAliases.get(0);
+        assertAll(() -> {
+            assertThat(certAliases).hasSize(1);
+            assertThat(certAlias).isNotEqualTo(keyAlias);
+        });
+
+        assertAll(() -> {
+            assertThat(ks.getCertificate(keyAlias)).isEqualTo(signed.getCertificate());
+            assertThat(ks.getCertificate(certAlias)).isEqualTo(issuer.getCertificate());
+            assertThat(ks.getType()).isEqualTo(signed.toKeyStore(password.toCharArray()).getType());
+        });
+    }
+
+    @NonNull
+    private List<String> aliasList(KeyStore ks) throws KeyStoreException {
+        List<String> aliases = new ArrayList<>();
+        ks.aliases().asIterator().forEachRemaining(aliases::add);
+        return aliases;
+    }
+
+    @NonNull
+    private List<String> keyAliasList(KeyStore ks) throws KeyStoreException {
+        return aliasList(ks).stream().filter(a -> {
+            try {
+                return ks.isKeyEntry(a);
+            }
+            catch (KeyStoreException e) {
+                throw new RuntimeException(e);
+            }
+        }).toList();
+    }
+
+    @NonNull
+    private List<String> certificateAliasList(KeyStore ks) throws KeyStoreException {
+        return aliasList(ks).stream().filter(a -> {
+            try {
+                return ks.isCertificateEntry(a);
+            }
+            catch (KeyStoreException e) {
+                throw new RuntimeException(e);
+            }
+        }).toList();
     }
 }
