c902fc0a1f7fdcdc969751091e5f7f84269904dd
Robert Young <robertyoungnz@gmail.com>
2026-01-27 15:26:01 +1300

    Add 4.2.0 to tests
    Also adds some sanity tests to check that we are adding the new kafka
    version to the test axes
    
    Signed-off-by: Robert Young <robertyoungnz@gmail.com>

--- integration-test/src/test/java/io/kroxylicious/testing/kafka/junit5ext/TestcontainersTest.java
+++ integration-test/src/test/java/io/kroxylicious/testing/kafka/junit5ext/TestcontainersTest.java
@@ -5,12 +5,17 @@
  */
 package io.kroxylicious.testing.kafka.junit5ext;
 
+import java.lang.reflect.Method;
+import java.util.List;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
+import java.util.stream.Stream;
 
+import org.apache.kafka.common.utils.AppInfoParser;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.platform.commons.util.ReflectionUtils;
 
 import io.kroxylicious.testing.kafka.common.Version;
 import io.kroxylicious.testing.kafka.testcontainers.TestcontainersKafkaCluster;
@@ -39,8 +44,30 @@ class TestcontainersTest {
     }
 
     @Test
+    void test42(@Version("4.2.0") TestcontainersKafkaCluster cluster) throws ExecutionException, InterruptedException, TimeoutException {
+        String clusterId = cluster.createAdmin().describeCluster().clusterId().get(10, TimeUnit.SECONDS);
+        assertThat(clusterId).isNotEmpty();
+    }
+
+    @Test
     void testLatest(@Version(Version.LATEST_RELEASE) TestcontainersKafkaCluster cluster) throws ExecutionException, InterruptedException, TimeoutException {
         String clusterId = cluster.createAdmin().describeCluster().clusterId().get(10, TimeUnit.SECONDS);
         assertThat(clusterId).isNotEmpty();
+    }
+
+    /**
+     * This test is here to check that the current kafka version on the classpath is covered
+     */
+    @Test
+    void testCurrentVersion() {
+        var versionFromClasspath = AppInfoParser.getVersion();
+        List<Method> methods = ReflectionUtils.findMethods(TestcontainersTest.class,
+                method -> method.getParameterCount() == 1 && method.getParameterTypes()[0] == TestcontainersKafkaCluster.class
+                        && method.getParameters()[0].isAnnotationPresent(Version.class));
+        List<String> allTestedVersions = methods.stream().flatMap(method -> Stream.of(method.getParameters()[0].getAnnotationsByType(Version.class))).map(Version::value)
+                .sorted()
+                .toList();
+        assertThat(allTestedVersions).withFailMessage("kafka version '" + versionFromClasspath + "' from classpath not found in tested versions " + allTestedVersions)
+                .contains(versionFromClasspath);
     }
 }
